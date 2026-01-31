59eba6ee28e7ef3dcc364fb2ec573174e283dfe7
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-01-30 23:04:46 -0600

    test

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-var cats = 100;
+var x = 50 //gobal 
 
+{
+  let y = 2;
+} //block
+
+let sum = x + y; // addition
+
+let answer1 = 'you got it';
+
+let answer2 = 'ummm..lets try again';
+
+const person = {firstName:"jimmy", lastName:"night", age:52, eyeColor:"black"};
 
 console.log ('hello hello test test')
